ffd90908b5fa368a7647ca43485db212097b21d8
MLaeeqKhan <lk7715714@gmail.com>
2024-04-09 12:41:20 +0500

    .

--- new-express-project/api/index.js
+++ new-express-project/api/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const connection = require("../DB/conn");
 
-app.get("/", (req, res) => res.send("Backend on Vercel"));
+
+
+
+
+
+app.get("/", (req, res) =>{
+    connection().then(() => {
+   
+    }).catch((err)=>{
+      console.error("Error connecting to the database:", err);
+    });
+    
+    res.send("Backend on Vercel");
+
+} 
+);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
