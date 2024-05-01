1865f7f07c066985cdb41ea0ae6b1e0fd9dfb164
MLaeeqKhan <lk7715714@gmail.com>
2024-05-01 22:02:59 +0500

    index.js

--- new-express-project/api/index.js
+++ new-express-project/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const connection = require("../DB/conn");
+// const connection = require("../DB/conn");
 
 // connection().then(() => {
    
